534a626ca30b6c9922aa845c9dd554f381849bd5
Michael De Leo <mdeleo10@hotmail.com>
2023-08-03 16:35:33 -0400

    Add private address support

--- vpngw.tf
+++ vpngw.tf
@@ -48,6 +48,7 @@ terraform {
     location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     allocation_method = "Dynamic"
+    sku               = "Standard"
   }
   
 # Create VPN GW
